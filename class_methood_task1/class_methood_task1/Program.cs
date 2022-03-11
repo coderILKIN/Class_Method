52fe8923cde1584d7fad02f8291e3e521c91e3a6
Ilkin Yusubov <tu758403c@code.edu.az>
2022-03-11 13:08:37 +0400

    1ci taks

---
+++ class_methood_task1/class_methood_task1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace class_methood_task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+            Student student = new Student();
+
+            student.Name = "Ilkin";
+            student.Surname = "Yusubov";
+
+            student.Group = "658a4";
+
+            student.Point = 80;
+            student.Graduated = true;
+
+
+            student.StudentInformation();
+
+
+
+
+        }
+    }
+    class Student
+    {
+        public string Name;
+        public string Surname;
+        public string Group;
+        public byte Point;
+        public Boolean Graduated;
+
+        public void StudentInformation()
+        {
+            Console.WriteLine($"Name: { Name}, Surname: { Surname}, Group: { Group}, Point: { Point}, Graduated: { Graduated}");
+        }
+
+    }
+}
